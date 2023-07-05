5d816ba10e897bc26e90da4d254e16b04583374d
Matheus Poda <61023281+matskira@users.noreply.github.com>
2023-07-05 23:07:16 +0000

    Corrigido a gaveta, tinha errado

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ function Home(){
         <h3>Siga os seguintes passos: </h3>
         <ol>
             <li>Vá até a cozinha</li>
-            <li>Abra a segunda gaveta do armário</li>
-            <li>Levante o pano amaralo</li>
+            <li>Abra a quarta gaveta do armário</li>
+            <li>Levante o pano amarelo</li>
             <li>Pegue o papel <strong>não abra até vir ao quarto</strong></li>
             <li>Leia em voz alta o que está escrito! Você irá receber um presentão</li>
         </ol>
