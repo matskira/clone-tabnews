3445363430718de184f63791f5aae7aa6c36a211
Matheus Poda <61023281+matskira@users.noreply.github.com>
2023-08-07 17:18:38 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto no qual irei estar adicionando todo meu aprendizado do curso.dev! Com o objetivo de aprender do zero como construir ideias e tornar real.
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-function Home(){
-    return (
-        <>
-        <h1>Sophy tenho um desafio a você!</h1>
-        <h3>Siga os seguintes passos: </h3>
-        <ol>
-            <li>Vá até a cozinha</li>
-            <li>Abra a quarta gaveta do armário</li>
-            <li>Levante o pano amarelo</li>
-            <li>Pegue o papel <strong>não abra até vir ao quarto</strong></li>
-            <li>Leia em voz alta o que está escrito! Você irá receber um presentão</li>
-        </ol>
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <h1>Sophy tenho um desafio a você!</h1>
+      <h3>Siga os seguintes passos: </h3>
+      <ol>
+        <li>Vá até a cozinha</li>
+        <li>Abra a quarta gaveta do armário</li>
+        <li>Levante o pano amarelo</li>
+        <li>
+          Pegue o papel <strong>não abra até vir ao quarto</strong>
+        </li>
+        <li>
+          Leia em voz alta o que está escrito! Você irá receber um presentão
+        </li>
+      </ol>
+    </>
+  );
 }
 
-
-export default Home
+export default Home;
